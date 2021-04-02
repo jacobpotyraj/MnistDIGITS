90c40c1f1bb058a402340b34902f9123292b8546
jacobpotyraj <48798986+jacobpotyraj@users.noreply.github.com>
2021-04-02 10:32:35 -0400

    Update Softmax.py

--- Softmax.py
+++ Softmax.py
@@ -4,7 +4,8 @@ Created on Thu Nov 14 16:19:55 2019
 
 @author: potyraj
 """
-
+# the last activation function of the CNN to normalize the output of the network
+# to a probability distribution over predicted output classes
 import numpy as np
 
 
